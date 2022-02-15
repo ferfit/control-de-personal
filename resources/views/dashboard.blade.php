2a239b25c9b6b4e7ca18e39b890680e1b25b1add
Fernando Pereyra <fpereyra@inxap.com>
2022-02-15 09:19:23 -0300

    agrega adminlte

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,37 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('adminlte::page')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+@section('title', 'Dashboard')
+
+@section('content_header')
+
+@stop
+
+@section('content')
+    <div class="row pt-5">
+
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box  bg-dark">
+                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">CPU Traffic</span>
+                    <span class="info-box-number">
+                        10
+                        <small>%</small>
+                    </span>
                 </div>
+
             </div>
         </div>
+
     </div>
-</x-app-layout>
+@stop
+
+@section('css')
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    {{-- <script>
+        console.log('Hi!');
+    </script> --}}
+@stop
